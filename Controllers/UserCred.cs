8a17875658656ee0ccf073e9d4da2df745704c0c
preeti_pandey <t.preeti1234@gmail.com>
2022-03-07 22:02:14 +0000

    JWT and CQRS
    JWT and CQRS

---
+++ Controllers/UserCred.cs
@@ -0,0 +1,15 @@
+﻿namespace Auth.Demo.Controllers
+{
+    public class UserCred
+    {
+        public string Username
+        {
+            get;
+            set;
+
+        }
+        public string Password { get; set; }
+
+
+    }
+}
